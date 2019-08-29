263148d9c0cec0dd0a66d02a1a8783606f33578b
Leonardo Alves <alves.leonardo.t@gmail.com>
2019-08-29 13:01:18 -0300

    Requirement 3

--- LearnOpenCV.py
+++ LearnOpenCV.py
@@ -22,51 +22,51 @@ def callback(value):
     choice = value
 
 def coloredImageClick(img, b, g, r):
-    print("Color values: R = " + str(r) + ", G = " + str(g) +  ", B = " + str(b))
     aux = img
     value_clicked = np.array([b, g, r])
     aux = np.square(np.subtract(aux.astype('int32'), value_clicked.astype('int32')))
-    aux = np.sum(aux, axis=2, keepdims=True)
-    aux = np.sqrt(aux)
-    aux = aux < 13
+    aux = np.sqrt(np.sum(aux, axis=2, keepdims=True)) < 13
     newimg = (np.where(aux, (0, 0, 255), img)).astype(img.dtype)
     cv2.imshow('New Image', newimg)
 
 def grayscaleImageClick(img, px):
-    print("Pixel intensity: " + str(px))
-    bools = (np.absolute(img.astype('int32') - px) < 13)
-    bools = np.reshape(bools,(img.shape[0], img.shape[1], 1))
-    newimg = np.reshape(img,(img.shape[0], img.shape[1], 1))
-    newimg = np.repeat(newimg, 3, axis=2)
+    bools = (np.absolute(img.astype('int32') - px) < 13).reshape((img.shape[0], img.shape[1], 1))
+    newimg = (np.reshape(img,(img.shape[0], img.shape[1], 1))).repeat(3, axis=2)
     newimg = (np.where(bools, (0, 0, 255), newimg)).astype(img.dtype)
     cv2.imshow('New Image', newimg)
 
-def imgClickEvent(event, x, y, flags, params):
+def ClickEvent(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print('Coordinates:\tx (column) = ' + str(x) + '\ty (row) = ' + str(y))
         if colored:
-            b, g, r = img[y, x]
-            coloredImageClick(img, b, g, r)
+            param.clear()
+            param.append(img[y, x][0])
+            param.append(img[y, x][1])
+            param.append(img[y, x][2])
+            print('Color values:\tR = ' + str(param[2]) + ',\tG = ' + str(param[1]) +  ',\tB = ' + str(param[0]))
+            coloredImageClick(img, param[0], param[1], param[2])
         else:
-            px = int(img[y, x])
-            grayscaleImageClick(img, px)
+            param.clear()
+            param.append(img[y, x])
+            print("Pixel intensity: " + str(param[0]))
+            grayscaleImageClick(img, param[0])
 
-b = tk.Radiobutton(master, text="Grayscale Image", variable=choice, command=lambda *args: callback("BWImg"))
-b.pack(anchor=tk.W)
-b = tk.Radiobutton(master, text="Color Image", variable=choice, command=lambda *args: callback("RGBImg"))
-b.pack(anchor=tk.W)
-b = tk.Radiobutton(master, text="Grayscale Video", variable=choice, command=lambda *args: callback("BWVid"))
-b.pack(anchor=tk.W)
-b = tk.Radiobutton(master, text="Color Video", variable=choice, command=lambda *args: callback("RGBVid"))
-b.pack(anchor=tk.W)
-b = tk.Radiobutton(master, text="Webcam Video", variable=choice, command=lambda *args: callback("CamVid"))
-b.pack(anchor=tk.W)
-b.wait_variable(choice)
+button = tk.Radiobutton(master, text="Grayscale Image", variable=choice, command=lambda *args: callback("BWImg"))
+button.pack(anchor=tk.W)
+button = tk.Radiobutton(master, text="Color Image", variable=choice, command=lambda *args: callback("RGBImg"))
+button.pack(anchor=tk.W)
+button = tk.Radiobutton(master, text="Grayscale Video", variable=choice, command=lambda *args: callback("BWVid"))
+button.pack(anchor=tk.W)
+button = tk.Radiobutton(master, text="Color Video", variable=choice, command=lambda *args: callback("RGBVid"))
+button.pack(anchor=tk.W)
+button = tk.Radiobutton(master, text="Webcam Video", variable=choice, command=lambda *args: callback("CamVid"))
+button.pack(anchor=tk.W)
+button.wait_variable(choice)
 
 isImg = True
 
 if "Img" in choice:
-    file_chosen = fd.askopenfilename(title="Choose a file", filetypes=[('image', ('.jpg', '.jpeg'))])
+    file_chosen = fd.askopenfilename(title="Choose a file", filetypes=[('image', ('.jpg', '.jpeg', '.png'))])
 elif "Vid" in choice:
     file_chosen = fd.askopenfilename(title="Choose a file", filetypes=[('video', ('.avi', '.264'))])
     isImg = False
@@ -76,6 +76,8 @@ else:
 
 flag = cv2.IMREAD_COLOR
 colored = True
+
+param = []
 
 if "BW" in choice:
     colored = False
@@ -87,7 +89,7 @@ if isImg:
         print("Error opening image")
         exit()
     cv2.imshow('Image', img)
-    cv2.setMouseCallback('Image', imgClickEvent)
+    cv2.setMouseCallback('Image', ClickEvent, param)
 
     print("Press 'q' to quit!")
 
@@ -110,6 +112,13 @@ elif file_chosen is not None:
             if not colored:
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             cv2.imshow('Video', img)
+            cv2.setMouseCallback('Video', ClickEvent, param)
+
+            if colored and param:
+                coloredImageClick(img, param[0], param[1], param[2])
+            elif param:
+                grayscaleImageClick(img, param[0])
+
             if frame_counter == vid.get(cv2.CAP_PROP_FRAME_COUNT):
                 frame_counter = 0
                 vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
