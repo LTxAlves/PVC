7ec7ac85321cc2371c3bcf01db1363ac132d40c7
Leonardo <alves.leonardo.t@gmail.com>
2019-09-26 13:31:50 -0300

    Read from calib.txt

--- src/StereoVision.py
+++ src/StereoVision.py
@@ -1,4 +1,22 @@
 from argparse import ArgumentParser
+import numpy as np
+
+def ReadMiddleburyTxt(path):
+    f = open(path, 'r')
+    text = f.readlines()
+    f.close()
+    newtext = []
+    for line in text:
+        newtext.append(line.replace('[', ' ').replace(';', '').replace(']', ' ').replace('=', ' '))
+
+    all_words = list(map(lambda l: l.split(), newtext))
+    intrinsics0 = np.array(all_words[0][1:], dtype='float').reshape((3, 3))
+    intrinsics1 = np.array(all_words[1][1:], dtype='float').reshape((3, 3))
+    doffs = float(all_words[2][1])
+    baseline = float(all_words[3][1])
+    ndisp = int(all_words[6][1])
+
+    return intrinsics0, intrinsics1, doffs, baseline, ndisp
 
 def Req1():
     while(True):
@@ -7,10 +25,20 @@ def Req1():
         print('\t2) Playroom')
         print('\t3) Quit')
         option = input('-> ')
-        if option == '1':
-            pass
+        if option == '1':    
+            intrinsics0, intrinsics1, doffs, baseline, ndisp = ReadMiddleburyTxt('../data/Middlebury/Piano-perfect/calib.txt')
+            print('intrinsics cam0 = \n', intrinsics0)
+            print('intrinsics cam1 = \n', intrinsics1)
+            print('doffs =', doffs)
+            print('baseline =', baseline)
+            print('ndisp =', ndisp)
         elif option == '2':
-            pass
+            intrinsics0, intrinsics1, doffs, baseline, ndisp = ReadMiddleburyTxt('../data/Middlebury/Playroom-perfect/calib.txt')
+            print('intrinsics cam0 = \n', intrinsics0)
+            print('intrinsics cam1 = \n', intrinsics1)
+            print('doffs =', doffs)
+            print('baseline =', baseline)
+            print('ndisp =', ndisp)
         elif option == '3':
             quit()
         else:
